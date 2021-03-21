c4481830a890bf93ae793f7a030b5877a8a3d45b
Velcorn <Velcorn@users.noreply.github.com>
2021-03-21 14:39:39 +0100

    Lots of improvements: - First check which images need processing - Adapt batch number based on number of images - Add exit conditions -> much more modular/adaptable

--- chalearn_jw.py
+++ chalearn_jw.py
@@ -7,16 +7,17 @@ Jan Willruth
 import argparse
 import glob
 import os
+import sys
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
 import gc
+
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import tensorflow as tf
 from keras.models import load_model
 from layers import BilinearUpSampling2D
 from utils_jw import predict, load_images, to_multichannel, resize_640, np2img, brightness
-
 
 gpus = tf.config.experimental.list_physical_devices("GPU")
 if gpus:
@@ -26,11 +27,11 @@ if gpus:
     except RuntimeError as e:
         print(e)
 
-
 # Argument Parser
 parser = argparse.ArgumentParser(description="High Quality Monocular Depth Estimation via Transfer Learning")
 parser.add_argument("--model", default="nyu.h5", type=str, help="Trained Keras model file.")
-parser.add_argument("--input", default="chalearn-input/*/*/*/*.jpg", type=str, help="Input filename or folder.")
+parser.add_argument("--input", default="chalearn-input/*/*/*/*.jpg", type=str, help="Input folder.")
+parser.add_argument("--output", default="chalearn-output/*/*/*/*.jpg", type=str, help="Output folder.")
 args = parser.parse_args()
 
 # Custom object needed for inference and training
@@ -41,17 +42,38 @@ resize = False
 if resize:
     print(resize_640("chalearn-input"))
 else:
-    print("\nSkipping resizing of images...")
+    print("Skipping resizing of images...")
+
+# Get images to process
+print("Getting images that need to be processed...")
+chalearn_input = glob.glob(args.input)
+chalearn_output = {x: 0 for x in glob.glob(args.output)}
+to_process = []
+for i in chalearn_input:
+    if i.replace("input", "output") not in chalearn_output:
+        to_process.append(i)
+num_of_images = len(to_process)
+
+# Exit program if no images to process, else continue
+if not to_process:
+    print(f"No images need to be processed, exiting...")
+    sys.exit()
+print(f"{num_of_images} images need to be processed, proceeding...")
 
 # Load model into GPU/CPU
 model = load_model(args.model, custom_objects=custom_objects, compile=False)
 print(f"\nModel loaded {args.model}.")
 
 # Current batch, batch size, total number of batches, left and right "borders" for batch
-# NOTE: batch size of 296 with batch number of 5630 seemed to be the sweet spot on my machine, results may vary greatly
-current_batch = 2229
+# NOTE: a batch size of 296 with a batch number of 5630 seemed to be the sweet spot on my machine,
+# results may vary greatly depending on your hardware specs!
+current_batch = 1
 batch_size = 296
-batch_number = 5630
+# Calculate batch number based on number of images to process.
+if num_of_images % batch_size == 0:
+    batch_number = num_of_images / batch_size
+else:
+    batch_number = num_of_images // batch_size + 1
 l = (current_batch - 1) * batch_size
 r = current_batch * batch_size
 for i in tqdm(range(batch_number - current_batch + 1)):
@@ -59,7 +81,11 @@ for i in tqdm(range(batch_number - current_batch + 1)):
 
     # Load images into memory
     print("Loading images into memory...")
-    names = glob.glob(args.input)[l:r]
+
+    # Update right border if number of remaining images is smaller than batch size
+    if len(to_process[l:]) < r:
+        r = len(to_process[l:])
+    names = to_process[l:r]
     inputs = load_images(names)
     print(f"Loaded {inputs.shape[0]} images of size {inputs.shape[1:]}.")
 
@@ -71,7 +97,7 @@ for i in tqdm(range(batch_number - current_batch + 1)):
         # Convert output to multichannel
         img = to_multichannel(item)
         # Convert from np array to image
-        img = np2img(np.uint8(img*255))
+        img = np2img(np.uint8(img * 255))
         # Brighten image
         # img = brightness(img, 1.5)
         # Get path to image and name
