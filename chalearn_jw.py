ec7037a9cd4bd00d253c580fb298e966e05bc609
Velcorn <Velcorn@users.noreply.github.com>
2021-05-17 16:29:55 +0200

    Everything seems to work

--- chalearn_jw.py
+++ chalearn_jw.py
@@ -19,6 +19,24 @@ from keras.models import load_model
 from layers import BilinearUpSampling2D
 from utils_jw import predict, load_images
 
+# JW: Limit memory usage to a fraction of total GPU memory.
+total_memory = 8000
+frac = 0.8
+limit = int(total_memory * frac)
+for gpu in tf.config.list_physical_devices('GPU'):
+    tf.config.experimental.set_virtual_device_configuration(gpu, [tf.config.experimental.VirtualDeviceConfiguration(
+        memory_limit=limit)])
+
+# Argument Parser
+parser = argparse.ArgumentParser(description="High Quality Monocular Depth Estimation via Transfer Learning")
+parser.add_argument("--model", default="nyu.h5", type=str, help="Trained Keras model file.")
+parser.add_argument("--input", default="chalearn-input/*/*/*/*.jpg", type=str, help="Input folder.")
+parser.add_argument("--output", default="chalearn-output/*/*/*/*.*", type=str, help="Output folder.")
+args = parser.parse_args()
+
+# Custom objects needed for inference and training
+custom_objects = {"BilinearUpSampling2D": BilinearUpSampling2D, "depth_loss_function": None}
+
 
 # Resizes images in path to 640x480
 def resize_640(path):
@@ -39,8 +57,8 @@ def estimate_depth():
     chalearn_output = {x: 0 for x in glob.glob(args.output)}
     to_process = []
     for i in chalearn_input:
-        img = i.replace("input", "output").replace("jpg", "npy")
-        if img not in chalearn_output:
+        name = i.replace("input", "output").replace("jpg", "npy")
+        if name not in chalearn_output:
             to_process.append(i)
     num_of_images = len(to_process)
     remaining = num_of_images
@@ -52,11 +70,11 @@ def estimate_depth():
 
     # Load model into GPU/CPU
     model = load_model(args.model, custom_objects=custom_objects, compile=False)
-    print(f"\nLoaded model {args.model}.")
+    print(f"Loaded model {args.model}")
 
     # Current batch, batch size, total number of batches, left and right "borders" for batch
-    # NOTE: a batch size of about 300-400 images seemed to be the sweet spot on my machine;
-    # results may vary greatly depending on your hardware specifications!
+    # NOTE: a batch size between 200-400 images seemed to be the sweet spot on my machine;
+    # results may vary greatly depending on your hardware specifications (mostly RAM, I suppose)!
     current_batch = 1
     batch_size = 400
     # Calculate batch number based on number of images to process.
@@ -67,7 +85,7 @@ def estimate_depth():
     l = (current_batch - 1) * batch_size
     r = current_batch * batch_size
     for i in tqdm(range(batch_number - current_batch + 1)):
-        print(f"\nProcessing batch {current_batch} of {batch_number}...\n")
+        print(f"\nProcessing batch {current_batch} of {batch_number}...")
 
         # Load images into memory
         print("Loading images into memory...")
@@ -82,7 +100,7 @@ def estimate_depth():
         # Compute results
         outputs = predict(model, inputs)
 
-        # Save results as to output folder
+        # Save depth estimation to output folder
         for j, item in enumerate(outputs.copy()):
             # Get path to image and name
             path = "/".join(names[j].replace("input", "output").split("\\")[:4])
@@ -90,7 +108,7 @@ def estimate_depth():
             # Create dir
             os.makedirs(path, exist_ok=True)
             # Resize image
-            img = cv2.resize(item, (320, 240))
+            name = cv2.resize(item, (320, 240))
             # Save image as numpy array of type float16 to save storage space
             '''
             NOTE: A float32 .npy file takes up 300 KB, resulting in ~600 GB with ~2 million images;
@@ -100,7 +118,7 @@ def estimate_depth():
             when saved as a .png image - might increase computation time of normalization though.
             Feel free to come up with a smarter solution ^^
             '''
-            np.save(f"{path}/{name}", img.astype(np.float16))
+            np.save(f"{path}/{name}", name.astype(np.float16))
 
         # Update variables
         current_batch += 1
@@ -127,9 +145,9 @@ def normalize():
     chalearn_output = {x: 0 for x in names}
     to_normalize = []
     for i in chalearn_input:
-        img = i.replace("input", "output")
-        if img not in chalearn_output:
-            to_normalize.append(img)
+        name = i.replace("input", "output").replace("jpg", "npy")
+        if name not in chalearn_output:
+            to_normalize.append(name)
     num_of_images = len(to_normalize)
 
     # Exit program if no images to process, else continue
@@ -138,56 +156,33 @@ def normalize():
     print(f"{num_of_images} images need to be normalized, proceeding...")
 
     print("Getting min-max across all images...")
-    min = np.inf
-    max = 0
-    for img in tqdm(names):
-        img = np.load(img)
-        lmin = np.min(img)
-        lmax = np.max(img)
-        if lmin < min:
-            min = lmin
-        if lmax > max:
-            max = lmax
+    min_val = np.inf
+    max_val = 0
+    for name in tqdm(names):
+        name = np.load(name)
+        min_img_val = np.min(name)
+        max_img_val = np.max(name)
+        if min_img_val < min_val:
+            min_val = min_img_val
+        if max_img_val > max_val:
+            max_val = max_img_val
 
     print("Normalizing and saving images...")
-    for j, img in tqdm(enumerate(names)):
+    for j, name in enumerate(tqdm(names)):
         # Get path to image and name
         path = "/".join(names[j].replace("input", "output").split("\\")[:4])
         name = names[j].split("\\")[4:][0][:-4]
         # Min-max normalize image to 0-255 range
         # (see https://stackoverflow.com/questions/48178884/min-max-normalisation-of-a-numpy-array)
-        img = np.load(f"{path}/{name}.npy")
-        img = (255.0 * (img - min) / (max - min)).astype(np.uint8)
+        name = np.load(f"{path}/{name}.npy")
+        name = (255.0 * (name - min_val) / (max_val - min_val)).astype(np.uint8)
         # Save normalized image as .jpg
-        cv2.imwrite(f"{path}/{name}.jpg", img)
+        cv2.imwrite(f"{path}/{name}.jpg", name)
 
-    '''print("Cleaning up...")
-    # Remove .npy files
-    for npy in names:
-        os.remove(npy)'''
-
-    print("All done!")
+    print("Finished normalizing images!")
 
 
 if __name__ == "__main__":
-    # JW: Limit memory usage to a fraction of total GPU memory.
-    total_memory = 8000
-    frac = 0.8
-    limit = int(total_memory * frac)
-    for gpu in tf.config.list_physical_devices('GPU'):
-        tf.config.experimental.set_virtual_device_configuration(gpu, [tf.config.experimental.VirtualDeviceConfiguration(
-            memory_limit=limit)])
-
-    # Argument Parser
-    parser = argparse.ArgumentParser(description="High Quality Monocular Depth Estimation via Transfer Learning")
-    parser.add_argument("--model", default="nyu.h5", type=str, help="Trained Keras model file.")
-    parser.add_argument("--input", default="chalearn-input/*/*/*/*.jpg", type=str, help="Input folder.")
-    parser.add_argument("--output", default="chalearn-output/*/*/*/*.*", type=str, help="Output folder.")
-    args = parser.parse_args()
-
-    # Custom objects needed for inference and training
-    custom_objects = {"BilinearUpSampling2D": BilinearUpSampling2D, "depth_loss_function": None}
-
     # Resize images if resize boolean True.
     resize = False
     if resize:
@@ -197,3 +192,10 @@ if __name__ == "__main__":
 
     print(estimate_depth())
     print(normalize())
+
+    print("Cleaning up...")
+    # Remove .npy files
+    for npy in tqdm(glob.glob("chalearn-output/*/*/*/*.npy")):
+        os.remove(npy)
+
+    print("All done!!!")
