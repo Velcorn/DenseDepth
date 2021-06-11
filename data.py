153e22f4a7cccf3f423a50601bbe6dced15b6805
Velcorn <Velcorn@users.noreply.github.com>
2021-06-11 19:10:56 +0200

    Try #1 of training on Frankenstein chalearn

--- data.py
+++ data.py
@@ -114,9 +114,9 @@ class NYU_BasicRGBSequence(Sequence):
 
             sample = self.dataset[index]
 
-            x = np.clip(np.asarray(Image.open(BytesIO(self.data[sample[0]]))).reshape(480, 640) / 255, 0, 1)
+            x = np.clip(np.asarray(Image.open(BytesIO(self.data[sample[0]]))).reshape(480, 640, 3) / 255, 0, 1)
             y = np.asarray(Image.open(BytesIO(self.data[sample[1]])),
-                           dtype=np.float32).reshape(480, 640).copy().astype(float) / 10.0
+                           dtype=np.float32).reshape(480, 640, 1).copy().astype(float) / 10.0
             y = DepthNorm(y, maxDepth=self.maxDepth)
 
             batch_x[i] = nyu_resize(x, 480)
